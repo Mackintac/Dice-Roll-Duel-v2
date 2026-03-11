c86a920e1d28dc3c04ab29baa4bb866516e96ec2
Mackintac <mack-robinson@hotmail.ca>
2026-03-11 00:43:38 -0400

    add documentation to explain hot-reload persistance

--- dice-roll-duel-v2/app/lib/db.ts
+++ dice-roll-duel-v2/app/lib/db.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client/extension';
 
+// checks for an existing PrismaClient in globalThis object
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// exports the cached version or a new PrismaClient if one doesn't already exist
 export const prisma = globalForPrisma ?? new PrismaClient();
 
+// update globalForPrisma to new version of PrismaClient if node environment is dev
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
