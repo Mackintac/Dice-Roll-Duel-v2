c21241bacf93868f1a4c4ff6ba9239eeff317e0d
Mackintac <mack-robinson@hotmail.ca>
2026-03-11 20:30:23 -0400

    add simple scorepip react component

--- dice-roll-duel-v2/app/components/ScorePips.tsx
+++ dice-roll-duel-v2/app/components/ScorePips.tsx
@@ -1,3 +1,16 @@
 interface ScorePipsProps {
   wins: number;
 }
+
+export default function ScorePips({ wins }: ScorePipsProps) {
+  return (
+    <div className='score-pips'>
+      {Array.from({ length: 2 }, (_, i) => (
+        <div
+          key={i}
+          className={`pip ${i < wins ? 'pip--filled' : 'pip--empty'}`}
+        />
+      ))}
+    </div>
+  );
+}
